2c3e69aa073525a6ee80c089074bbd543c3a91b2
Brug-buo <bruna.valera.marques@escola.pr.gov.br>
2026-09-14 20:55:51 -0300

    10
    Add comments explaining import syntax for Botao component

---
+++ EXs-Bruna/aula 01/ex10.js
@@ -0,0 +1,1 @@
+//O primeiro (import Botao) importa o componente que foi exportado como padrão (export default), enquanto o segundo (import { Botao }) importa uma exportação nomeada específica (export const Botao).
