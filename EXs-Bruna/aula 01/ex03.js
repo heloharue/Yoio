cd0d4102e41eb81bda50b47cc83aada28fb6c186
Brug-buo <bruna.valera.marques@escola.pr.gov.br>
2026-09-14 20:45:57 -0300

    3
    Add a comment explaining the map and filter methods.

---
+++ EXs-Bruna/aula 01/ex03.js
@@ -0,0 +1,1 @@
+//O map transforma cada elemento de um array gerando um novo array do mesmo tamanho, enquanto o filter seleciona apenas os elementos que passam em um teste, gerando um array menor ou igual.
