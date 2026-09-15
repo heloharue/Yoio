6b161bc23f354a920cd52907c4b98d84557a21f6
Brug-buo <bruna.valera.marques@escola.pr.gov.br>
2026-09-14 21:25:12 -0300

    12
    Removed unused variable 'emEstoque' and cleaned up code.

--- EXs-Bruna/aula 01/ex12.js
+++ EXs-Bruna/aula 01/ex12.js
@@ -1,4 +1,3 @@
-const obterNome = produto => produto.nome
+const obterNome = produto => produto.nome;  
 
-const emEstoque = produtos.filter(p => p.estoque > 0).map(obterNome)
-const estoqueMaiorQue5 = produtos.filter(p => p.estoque > 5).map(obterNome);
+const estoqueMaiorQue5 = produtos.filter(p => p.estoque > 5).map(obterNome);   
