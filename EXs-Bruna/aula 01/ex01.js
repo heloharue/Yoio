8d551d6feff8b677ccf74f109cd4d00ac317c5c5
Brug-buo <bruna.valera.marques@escola.pr.gov.br>
2026-09-14 21:24:49 -0300

    1

--- EXs-Bruna/aula 01/ex01.js
+++ EXs-Bruna/aula 01/ex01.js
@@ -1,1 +1,57 @@
 const dobro = nums.map(n => n * 2);
+
+
+
+
+
+
+//CODIGO COMPLETO COM TODOS OS EXERCICIOS 
+//segue abaixo caso queira complrto o codigo para rodar
+//const nums =; 
+
+//const p = { titulo: "Caneca", preco: 25 };
+//const cores = ["azul", "verde"];
+//const produtos = [   
+  //{ nome: "Caneca", estoque: 3 },   
+  //{ nome: "Camiseta", estoque: 0 },   
+ // { nome: "Adesivo", estoque: 7 } 
+//];  
+
+// 1. Resultado [2, 4, 6, 8] 
+//const resultado1 = nums.map(n => n * 2);  
+
+// 2. Produzir só os números pares  
+//const resultado2 = nums.filter(n => n % 2 === 0);  
+
+// 4. Desestruturar titulo e preco de 'p' 
+//const { titulo, preco } = p;  
+
+// 5. Função 'ehCaro' 
+//const ehCaro = (valor) => valor > 100;  
+
+// 6. Criar 'cores2' com "vermelho" no fim, sem alterar 'cores' 
+//const cores2 = [...cores, "vermelho"];  
+
+// 7. Criar 'pEmPromocao' com preço 20, sem alterar 'p' 
+//const pEmPromocao = { ...p, preco: 20 };  
+
+// 8. Só os nomes dos produtos em estoque (estoque > 0)
+//const nomesEmEstoque = produtos   
+ // .filter(produto => produto.estoque > 0)   
+  //.map(produto => produto.nome);  
+
+// 12. Nomes dos produtos com estoque maior que 5 
+//const obterNome = produto => produto.nome;  
+//const estoqueMaiorQue5 = produtos.filter(p => p.estoque > 5).map(obterNome);   
+
+// Saídas no Console
+//console.log("1:", resultado1); 
+//console.log("2:", resultado2);  
+//console.log("4:", titulo, preco);  
+//console.log("5:", ehCaro(150), ehCaro(50)); 
+//console.log("6: Original:", cores, "| Novo:", cores2);  
+//console.log("7: Original:", p, "| Novo:", pEmPromocao); 
+//console.log("8:", nomesEmEstoque); 
+//console.log("12:", estoqueMaiorQue5);
+
+
