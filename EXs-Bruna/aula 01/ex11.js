0412a68d6b36832084e0008f80d123e524e8cbb8
Brug-buo <bruna.valera.marques@escola.pr.gov.br>
2026-09-14 21:05:27 -0300

    11
    Add comment explaining React immutability principle

---
+++ EXs-Bruna/aula 01/ex11.js
@@ -0,0 +1,1 @@
+//O React baseia-se no princípio da imutabilidade, e o push modifica o array original diretamente na memória, o que impede o React de detectar a mudança e atualizar a tela (renderizar).
